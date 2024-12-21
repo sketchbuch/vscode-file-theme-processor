fade564c48c7da711ca0f40531dcb6fc53ee1d57
sketchbuch <dev.sketchbuch@gmail.com>
2024-12-21 14:17:17 +0100

    feat(1.0.7): updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.0.7](https://github.com/sketchbuch/vscode-file-theme-processor/compare/v1.0.5...v1.0.7) (2024-12-21)
+
+- Closes [#1](https://github.com/sketchbuch/vscode-file-theme-processor/issues/1) **Default Fileicons are not loading anymore**
+
 ## [1.0.6](https://github.com/sketchbuch/vscode-file-theme-processor/compare/v1.0.5...v1.0.6) (2024-12-16)
 
 - Closes [#1](https://github.com/sketchbuch/vscode-file-theme-processor/issues/1) **Default Fileicons are not loading anymore**
--- src/utils/theme/getActiveExtThemeData.ts
+++ src/utils/theme/getActiveExtThemeData.ts
@@ -24,7 +24,7 @@ export const getDefaultExtThemeData = (activeFileiconTheme: string): ExtThemeDat
 
   try {
     defTheme: for (const ext of vscode.extensions.all) {
-      if (ext.packageJSON?.contributes.iconThemes) {
+      if (ext.packageJSON.contributes && ext.packageJSON.contributes.iconThemes) {
         const iconThemes = ext.packageJSON.contributes.iconThemes as ThemeInfo[]
 
         for (const themeInfo of iconThemes) {
@@ -72,7 +72,7 @@ export const getUserExtThemeData = async (activeFileiconTheme: string): Promise<
             const packageJsonContent = await fs.promises.readFile(packageJsonPath, 'utf8')
             const packageJson = JSON5.parse(packageJsonContent)
 
-            if (packageJson?.contributes.iconThemes) {
+            if (packageJson.contributes && packageJson.contributes.iconThemes) {
               const iconThemes = packageJson.contributes.iconThemes as ThemeInfo[]
 
               for (const themeInfo of iconThemes) {
